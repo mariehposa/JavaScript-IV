7ef75f9175a174ea22a411baf6f95d8fca24686a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-08-22 15:51:58 +0100

    created class Instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,18 @@ class Person {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
     }
 }
+
+class Instructor extends Person{
+    constructor(object){
+        super(object);
+        this.specialty = object.specialty;
+        this.favLanguage = object.favLanguage;
+        this.catchPhrase = object.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student, subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
