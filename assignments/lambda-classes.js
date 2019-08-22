e7c53f28666638bda64a785b7377d4c474d88e63
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-08-22 15:42:16 +0100

    created class Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(object){
+        this.name = object.name;
+        this.age = object.age;
+        this.location = object.location;
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    }
+}
