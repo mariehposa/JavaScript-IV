a8bda3f061c0e75f27a31d4053f1f041202b4dbb
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-08-22 13:54:54 +0100

    copy in solution from yesterday

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -4,6 +4,89 @@ Prototype Refactor
 
 1. Copy and paste your code or the solution from yesterday
 
+
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+
+Person.prototype.eat = function (edible) {
+  this.stomach.push(edible)
+}
+
+Person.prototype.poop = function () { 
+  this.stomach = [] 
+}
+
+const mariam = new Person("Mariam", 23)
+
+mariam.eat('rice')
+
+console.log(mariam)
+
+mariam.poop()
+
+console.log(mariam)
+
+//Task 2
+function Car(model, carMake){
+  this.model = model;
+  this.carMake = carMake;
+  this.odometer = 0;
+  this.crashed = false;
+}
+
+Car.prototype.drive = function (distance) {
+  if(this.crashed)
+    return `I crashed at ${this.odometer} miles!`
+  this.odometer += Number(distance)
+  return `I did not crash and i am at ${this.odometer} miles now`;
+}
+
+Car.prototype.crash = function () {
+  this.crashed = true;
+}
+
+Car.prototype.repair = function () { this.crashed = false; }
+
+var aCar = new Car('YC', 'BMW');
+
+//Task 3
+function Baby(name, age) {
+  Person.call(this, name, age)
+}
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function () { return `I love to play and I am ${this.age} years old...`}
+
+var mine = new Baby('Mine', 3)
+
+//Task 4
+function Truck(age, type){
+  this.age = age;
+  this.type = type;
+  this.tankLevel = 10;
+  this.miles = 0;
+}
+
+Truck.prototype.drive = function (miles) {
+  if(Number(miles) <= this.tankLevel){
+    this.miles += Number(miles)
+    return `We have covered ${miles} miles`
+  }
+
+  return `Fuels tank is not sufficient, maybe refill.`
+}
+
+Truck.prototype.refill = function (litres) { this.tankLevel += Number(litres)}
+
+var truck = new Truck(2, 'Toyota')
+*/
+
+
+/*
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
